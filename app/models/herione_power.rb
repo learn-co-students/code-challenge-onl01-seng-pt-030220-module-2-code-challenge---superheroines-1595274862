719834316a4443caf177ec7290d66402f4d9bc26
jordles113 <jordan.t.romero@gmail.com>
2020-07-21 13:40:00 -0500

    commit at 1.5 hours

---
+++ app/models/herione_power.rb
@@ -0,0 +1,4 @@
+class HerionePower < ApplicationRecord
+  belongs_to :power
+  belongs_to :herione
+end
---
+++ db/migrate/20200721170818_create_herione_powers.rb
@@ -0,0 +1,9 @@
+class CreateHerionePowers < ActiveRecord::Migration[5.1]
+  def change
+    create_table :heroine_powers do |t|
+      t.references :power, foreign_key: true
+      t.references :herione, foreign_key: true
+      t.timestamps
+    end
+  end
+end
