60f0e3b143572d4f61ebe288f2d7e89670042865
Christopher Chaison <christopherc0825@gmail.com>
2020-07-20 20:56:27 -0400

    completing 1 and a half hours code challenge

--- app/controllers/heroines_controller.rb
+++ app/controllers/heroines_controller.rb
@@ -1,5 +1,45 @@
 class HeroinesController < ApplicationController
+  before_action :set_heroine!, only: [:show, :edit, :update, :destroy]
+
   def index
     @heroines = Heroine.all
+  end
+
+  def show
+  end
+
+  def new
+    @heroine = Heroine.new
+  end
+
+  def create
+    Heroine.create(heroine_params)
+    redirect_to heroines_path
+  end
+
+  def edit
+  end
+
+  def update
+    if @heroine.update(heroine_params)
+      redirect_to @heroine
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @heroine.destroy
+    redirect_to heroines_url
+  end
+
+  private 
+  
+  def heroine_params
+    params.require(:heroine).permit(:name, :super_name)
+  end
+
+  def set_heroine!
+    @heroine = Heroine.find(params[:id])
   end
 end
--- app/controllers/powers_controller.rb
+++ app/controllers/powers_controller.rb
@@ -1,5 +1,45 @@
 class PowersController < ApplicationController
+  before_action :set_power!, only: [:show, :edit, :update, :destroy]
+
   def index
     @powers = Power.all
+  end
+
+  def show
+  end
+
+  def new
+    @power = Power.new
+  end
+
+  def create
+    Power.create(power_params)
+    redirect_to @power
+  end
+
+  def edit
+  end
+
+  def update
+    if @power.update(power_params)
+      redirect_to @power
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @power.destroy
+    redirect_to powers_url
+  end
+
+  private 
+  
+  def power_params
+    params.require(:power).permit(:name, :description)
+  end
+
+  def set_power!
+    @power = Power.find(params[:id])
   end
 end
