01dbd786122ce27b28f08578d34988b965b64455
chrisb3546 <chrisb3546@yahoo.com>
2020-07-23 15:54:02 -0400

    at an hour and a half

--- app/controllers/powers_controller.rb
+++ app/controllers/powers_controller.rb
@@ -2,4 +2,13 @@ class PowersController < ApplicationController
   def index
     @powers = Power.all
   end
+
+  def show 
+    @power = Power.find_by(id: params[:id])
+  end
+
+  def new
+    @power = Power.new
+  end
+
 end
